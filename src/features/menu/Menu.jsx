acd1736b5bf463d65af2efcf0f4d7a738e6577f4
Muhammad Waleed <mwaleedkhan124@gmail.com>
2025-08-24 22:39:31 +0500

    Basic routing and data fetching

---
+++ src/features/menu/Menu.jsx
@@ -0,0 +1,27 @@
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
+
+function Menu() {
+  const menu = useLoaderData();
+  return (
+    <ul>
+      {menu.map((pizza) => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
+  );
+}
+
+export async function loader() {
+  const res = await fetch("https://react-fast-pizza-api.jonas.io/api/menu");
+
+  if (!res.ok) {
+    throw Error("Failed getting menu");
+  } else {
+    const {data}  = await res.json();
+    return data;
+  }
+}
+
+export default Menu;
