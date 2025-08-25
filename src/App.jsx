c748d198231fda201e36e65e4f96bf0927adedbf
Muhammad Waleed <mwaleedkhan124@gmail.com>
2025-08-25 23:15:31 +0500

    Loading Data from backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./ui/Home";
+import Error from "./ui/Error";
 import Menu, {loader as menuLoader} from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
-import Order from "./features/order/Order";
+import Order, {loader as orderLoader} from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       {
         index: true,
@@ -19,6 +21,7 @@ const router = createBrowserRouter([
         path: "/menu",
         element: <Menu />,
         loader: menuLoader,
+        errorElement: <Error />,
       },
       {
         path: "/cart",
@@ -31,6 +34,8 @@ const router = createBrowserRouter([
       {
         path: "/order/:orderId",
         element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
       },
     ],
   },
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import SearchOrder from '../features/order/SearchOrder'
 
 function Header() {
   return (
     <header>
         <Link to="/">Fast Pizze Co.</Link>
+        <SearchOrder />
         <p>Waleed</p>
     </header>
   )
