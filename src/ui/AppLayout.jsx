3b69a2eceb3a3409c1537004f33f211cadc0a942
Muhammad Waleed <mwaleedkhan124@gmail.com>
2025-08-27 20:14:36 +0500

    styling done

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,23 +1,27 @@
-import Header from "./Header"
-import CartOverview from '../features/cart/CartOverview'
-import { Outlet, useNavigation } from "react-router-dom"
+import Header from "./Header";
+import CartOverview from "../features/cart/CartOverview";
+import { Outlet, useNavigation } from "react-router-dom";
 import Loader from "./Loader";
 
 function AppLayout() {
   const navigation = useNavigation();
 
-  const isLoading = navigation.state === 'loading';
+  const isLoading = navigation.state === "loading";
   return (
-    <div className="layout">
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
-        <Header />
-        <main>
-            <Outlet />
-        </main>
 
-        <CartOverview />
+      <Header />
+
+      <div className="overflow-scroll">
+        <main className="max-w-3xl mx-auto">
+          <Outlet />
+        </main>
+      </div>
+
+      <CartOverview />
     </div>
-  )
+  );
 }
 
-export default AppLayout
+export default AppLayout;
